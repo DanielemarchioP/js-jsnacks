d340662a1681eb6cdd3fc2d4d8854f551c7988c7
DanieleM <danielemarchio709@gmail.com>
2024-03-16 01:13:09 +0100

    ADD: ESERCIZI FINO AL 6

---
+++ js/app.js
@@ -0,0 +1,149 @@
+// console.log('JAVA SNAK')
+
+// const num1 = parseInt(prompt('inserisci il primo numero da confrontare'))  //NUM|NULL
+// console.log(num1)
+// const num2 = parseInt(prompt('inserisci il secondo numero da confrontare'))  //NUM|NULL
+// console.log(num2)
+
+
+// if(num1 > num2){
+//     console.log(num1 , 'e maggiore di ', num2)
+// }
+// else{
+    
+//         console.log(num2 , 'e maggiore o uguale di ', num1)
+//     }
+
+
+
+// JSNAK 2
+
+// const parola1 = prompt('inserisci la prima parola da confrontare') //string|null
+// const parola2 = prompt('inserisci la seconda parola da confrontare') //string|null
+
+// const parola1Lunghezza = parola1.length
+// console.log(parola1Lunghezza)
+
+// const parola2Lunghezza = parola2.length
+// console.log(parola2Lunghezza)
+
+
+//     if(parola1Lunghezza > parola2Lunghezza){
+//         console.log(parola2 , parola1)
+//     }
+    
+//     else{
+//         console.log(parola1 , parola2)
+//     }
+
+
+
+
+//JSNAK3
+
+// let somma = 0
+
+// for(i = 0; i<10 ; i++){
+//     let num = parseInt(prompt('inserisci i numeri da sommare'))
+//     somma = somma + num
+//     console.log(num, somma)
+
+// }
+
+// console.log(somma)
+
+
+
+
+// JSNAK4
+
+// const invitati = ['daniele', 'mary' , 'gianluca' , 'claudia']
+// console.log(invitati)
+// const nome = prompt('inserisci il tuo nome per il controllo della lista')
+// console.log(nome)
+
+// const invitatiNum = invitati.length
+// console.log('il numero di invitati e ' , invitatiNum)
+
+// for(i=0 ; i < invitatiNum ; i++){
+//     if(invitati[i] === nome){
+//         console.log('puoi entrare')
+//         break;
+//     }
+//     else if(invitati[i] !== nome ){
+//         console.log('non puoi entrare')
+//     }
+// }
+
+// console.log()
+
+
+
+
+
+//JSNAK5
+
+// const num = []
+
+// for(i = 0 ; i < 6 ; i++){
+
+//     const test = parseInt(prompt('inserisci un numero'))
+    
+//     const testDispari = test % 2
+
+//     if(testDispari !== 0){
+//         num.push(test)
+//     }
+
+    
+// }
+
+// console.log(num)
+
+
+
+//JSNAKS6
+
+// let num = Math.floor(Math.random()*10 + 1)
+
+// console.log(num)
+
+// const numUtente =  parseInt(prompt('inserisci un numero compreso tra 1 e 10'))
+// console.log(numUtente)
+
+//     if(num === numUtente){
+//        console.log('HAI VINTOO STOCA')
+//     }
+//     else{
+//         console.log('HAI PERSO LOL')
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
